6827ca9cffc80fedbe88370be004bc82b4255780
Enpas <enniopas2002@libero.it>
2021-06-09 19:32:13 +0200

    completo

--- api.php
+++ api.php
@@ -2,6 +2,33 @@
 
   include __DIR__ . '/data/db.php';
 
+$arrGenres = [];
+$arrDiscs = empty($_GET['genre']) || $_GET['genre'] === 'all' ? $database : [];
+
+foreach($database as $disc){
+
+  if(!in_array($disc['genre'],$arrGenres)){
+    $arrGenres[] = $disc['genre'];
+  }
+
+}
+
+if(count($arrDiscs) === 0){
+
+  foreach($database as $disc){
+    if($disc['genre'] === $_GET['genre']){
+      $arrDiscs[] = $disc;
+    }
+  }
+
+}
+
+
+$response = [
+  'arrDiscs' => $arrDiscs,
+  'arrGenres' => $arrGenres
+];
+
   header('Content-Type: application/json');
 
   echo json_encode($database);
--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
 const app = new Vue({
   el: '#app',
 
-  data: {
-    arrDiscs: [],
-  },
+  data:{
 
-  mounted() {
-    axios.get('http://localhost/php-ajax-dischi/api.php')
+    arrDiscs:[],
+    arrGenres:[],
+    searchGenre: 'all',
+    apiURL: 'http://localhost/dischi-php-ajax/api.php'
+
+  },
+  methods:{
+
+    getAPI(){
+      axios.get(this.apiURL,{
+        params:{
+          genre: this.searchGenre
+        }
+      })
       .then(res => {
-        this.arrDiscs = res.data;
-      })
-      .catch(err => {
+
+        this.arrDiscs = res.data.arrDiscs;
+        this.arrGenres = res.data.arrGenres;
+        console.log(this.arrGenres);
+
+      }).catch(err => {
+
         console.log(err);
+
       })
+    }
+
+  },
+  created(){
+    this.getAPI();
   }
 
-})
+});
